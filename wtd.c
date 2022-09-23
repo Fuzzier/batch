ac089efe39b5c6af4d3510ecda3dc6ed986e1b1f
Wei Tang <gauchyler@uestc.edu.cn>
2022-09-23 11:57:02 +0800

    Updated `wtd.c`. * `wt.exe` is now launched directly. * To build `wtd.exe`, use any of the following methods:   ```clang-cl /Ox /MT /DNDEBUG wtd.c```   ```cl /Ox /MT /DNDEBUG wtd.c```

--- wtd.c
+++ wtd.c
@@ -5,100 +5,122 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#pragma comment(lib, "advapi32")  // RegXxx()
+
 int main(void)
 {
-    DWORD n = GetCurrentDirectory(0, NULL);
-    if (n == 0)
+    DWORD retval = 0;
+    LPTSTR cmdline = NULL;
+    HKEY hkey = NULL;
+    LPTSTR app = NULL;
+    do // while (false)
     {
-        n = GetLastError();
-        _ftprintf(stderr, _T("Cannot get length of current directory 0x%08lx.\n"), n);
-        return n;
+        ////////////////////
+        // The current directory.
+        DWORD n = GetCurrentDirectory(0, NULL);
+        if (n == 0)
+        {
+            retval = GetLastError();
+            _ftprintf(stderr, _T("Cannot get length of current directory 0x%08lx.\n"), retval);
+            break;
+        }
+        n += 50; // Allocate extra characters.
+        cmdline = (LPTSTR)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, n * sizeof(TCHAR));
+        if (cmdline == NULL)
+        {
+            retval = GetLastError();
+            _ftprintf(stderr, _T("Cannot allocate heap memory 0x%08lx.\n"), retval);
+            break;
+        }
+        // " -d <directory>"
+        LPTSTR p = cmdline;
+        *p++ = ' '; // Need one more space, or "wt.exe" would compliant that it cannot launch the path.
+        *p++ = '-';
+        *p++ = 'd';
+        *p++ = ' ';
+        *p++ = '"';
+        n = GetCurrentDirectory(n, p);
+        if (n == 0)
+        {
+            retval = GetLastError();
+            _ftprintf(stderr, _T("Cannot get current directory 0x%08lx.\n"), retval);
+            break;
+        }
+        p += n;
+        // Remove the trailing backslash, if any.
+        if (*(p - 1) == '\\')
+        {
+            --p;
+        }
+        *p++ = '"';
+        *p++ = '\0';
+        ////////////////////
+        // Query registry for "wt.exe".
+        retval = RegOpenKeyEx(HKEY_CURRENT_USER,
+                              _T("Software\\Microsoft\\Windows\\CurrentVersion\\App Paths\\wt.exe"),
+                              0, // Options
+                              KEY_QUERY_VALUE,
+                              &hkey);
+        if (retval != ERROR_SUCCESS)
+        {
+            retval = GetLastError();
+            _ftprintf(stderr, _T("Cannot open registry '%s', 0x%08lx.\n"),
+                      _T("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\App Paths\\wt.exe"),
+                      retval);
+            break;
+        }
+        LONG length = 0;
+        retval = RegQueryValue(hkey, NULL, NULL, &length);
+        if (retval != ERROR_SUCCESS)
+        {
+            break;
+        }
+        app = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, length);
+        if (app == NULL)
+        {
+            retval = GetLastError();
+            _ftprintf(stderr, _T("Cannot allocate heap memory 0x%08lx.\n"), retval);
+            break;
+        }
+        retval = RegQueryValue(hkey, NULL, app, &length);
+        if (retval != ERROR_SUCCESS)
+        {
+            break;
+        }
+        ////////////////////
+        // Output diagnostic information.
+#if !defined(NDEBUG)
+        _ftprintf(stdout, _T("%s %s\n"), app, cmdline);
+#endif // !defined(NDEBUG)
+        ////////////////////
+        // Run Windows Terminmal.
+        STARTUPINFO startupInfo;
+        memset(&startupInfo, 0, sizeof(startupInfo));
+        startupInfo.cb = sizeof(STARTUPINFO);
+        PROCESS_INFORMATION processInfo;
+        memset(&processInfo, 0, sizeof(processInfo));
+        n = CreateProcess(app,           // Application
+                          cmdline,       // Command line arguments
+                          NULL,          // Process attributes
+                          NULL,          // Thread attributes
+                          FALSE,         // Inherit handles
+                          0,             // Flags
+                          NULL,          // Environment
+                          NULL,          // Current directory
+                          &startupInfo,  // Startup info
+                          &processInfo); // Process info
+        if (n == 0)
+        {
+            retval = GetLastError();
+            _ftprintf(stderr, _T("Cannot create process 0x%08lx.\n"), retval);
+            break;
+        }
+        CloseHandle(processInfo.hThread);
+        CloseHandle(processInfo.hProcess);
     }
-    n += 50;
-    LPTSTR buf = (LPTSTR)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, n * sizeof(TCHAR));
-    if (buf == NULL)
-    {
-        n = GetLastError();
-        _ftprintf(stderr, _T("Cannot allocate heap memory 0x%08lx.\n"), n);
-        return n;
-    }
-    // "cmd.exe /c wt -d <directory>"
-    LPTSTR p = buf;
-    *p++ = '/';
-    *p++ = 'c';
-    *p++ = ' ';
-    *p++ = 'w';
-    *p++ = 't';
-    *p++ = ' ';
-    *p++ = '-';
-    *p++ = 'd';
-    *p++ = ' ';
-    *p++ = '"';
-    n = GetCurrentDirectory(n, p);
-    if (n == 0)
-    {
-        n = GetLastError();
-        _ftprintf(stderr, _T("Cannot get current directory 0x%08lx.\n"), n);
-        return n;
-    }
-    p += n;
-    // Remove the trailing backslash, if any.
-    if (*(p - 1) == '\\')
-    {
-        --p;
-    }
-    *p++ = '"';
-    *p++ = '\0';
-
-    _ftprintf(stdout, _T("cmd.exe %s\n"), buf);
-    //*
-    STARTUPINFO startupInfo;
-    memset(&startupInfo, 0, sizeof(startupInfo));
-    startupInfo.cb = sizeof(STARTUPINFO);
-    PROCESS_INFORMATION processInfo;
-    memset(&processInfo, 0, sizeof(processInfo));
-    n = CreateProcess(_T("C:\\Windows\\System32\\cmd.exe"), // Application
-                      buf,           // Command line arguments
-                      NULL,          // Process attributes
-                      NULL,          // Thread attributes
-                      FALSE,         // Inherit handles
-                      0,             // Flags
-                      NULL,          // Environment
-                      NULL,          // Current directory
-                      &startupInfo,  // Startup info
-                      &processInfo); // Process info
-    if (n == 0)
-    {
-        n = GetLastError();
-        _ftprintf(stderr, _T("Cannot create process 0x%08lx.\n"), n);
-        HeapFree(GetProcessHeap(), 0, buf);
-        return n;
-    }
-    CloseHandle(processInfo.hThread);
-    CloseHandle(processInfo.hProcess);
-    //*/
-
-    /*
-#pragma comment(lib, "ole32")
-#pragma comment(lib, "shell32.lib")
-    CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-    HINSTANCE h = ShellExecute(NULL,          // HWND
-                               _T("open"),    // Verb
-                               _T("cmd.exe"), // File
-                               buf,           // Command line arguments
-                               NULL,          // Directory
-                               SW_HIDE);      // ShowCmd
-    if ((INT_PTR)(h) <= 32)
-    {
-        n = GetLastError();
-        _ftprintf(stderr, _T("Cannot create process 0x%08lx.\n"), n);
-        CoUninitialize();
-        HeapFree(GetProcessHeap(), 0, buf);
-        return n;
-    }
-    CoUninitialize();
-    */
-
-    HeapFree(GetProcessHeap(), 0, buf);
-    return 0;
+    while (0);
+    HeapFree(GetProcessHeap(), 0, cmdline);
+    RegCloseKey(hkey);
+    HeapFree(GetProcessHeap(), 0, app);
+    return retval;
 }
