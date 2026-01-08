7c2ff02e6ad368445a34f9a1fe6251c926b91b7d
Wei Tang <gauchyler@uestc.edu.cn>
2026-01-08 12:13:46 +0800

    Updated `SetClangEnv.cmd`
    * Added path for `Ninja`.

--- SetClangEnv.cmd
+++ SetClangEnv.cmd
@@ -15,7 +15,8 @@ REM   All rights reserved.
 REM ===========================================================
 
 SET __clang_root__=D:\Development\Tools\pub\LLVM
-SET __cmake_root__=D:\Documents\CMake
+SET __cmake_bin__=D:\Documents\Make\CMake\bin
+SET __ninja_bin__=D:\Documents\Make\Ninja
 
 FOR %%i IN (clang.exe) DO (
     SET __found_clang__=%%~$PATH:i
@@ -33,7 +34,7 @@ IF DEFINED __found_clang__ (
 )
 
 IF NOT DEFINED __found_clang__ (
-    ECHO Add path:  %__clang_root__%\bin
+    ECHO Add path: %__clang_root__%\bin
     CALL Envvar :EnvvarPathAppend "PATH"    "%__clang_root__%\bin"
 )
 
@@ -49,8 +50,16 @@ FOR %%i IN (cmake.exe) DO (
     SET __found_cmake__=%%~$PATH:i
 )
 IF NOT DEFINED __found_cmake__ (
-    ECHO Add path: %__cmake_root__%\bin
-    CALL Envvar :EnvvarPathAppend "PATH"    "%__cmake_root__%\bin"
+    ECHO Add path: %__cmake_bin__%\
+    CALL Envvar :EnvvarPathAppend "PATH"    "%__cmake_bin__%"
+)
+
+FOR %%i IN (ninja.exe) DO (
+    SET __found_ninja__=%%~$PATH:i
+)
+IF NOT DEFINED __found_ninja__ (
+    ECHO Add path: %__ninja_bin__%\
+    CALL Envvar :EnvvarPathAppend "PATH"    "%__ninja_bin__%"
 )
 
 :Exit
@@ -58,5 +67,6 @@ SET __found_clang__=
 SET __found_clang_lib__=
 SET __clang_ver__=
 SET __found_cmake__=
+SET __found_ninja__=
 
 EXIT /B
